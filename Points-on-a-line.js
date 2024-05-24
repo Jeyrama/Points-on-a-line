7a2d36b3b67fde46aeb4735826a60bc85cfd4e98
Jey <126204426+Jeyrama@users.noreply.github.com>
2024-05-23 19:07:26 -0700

    Update Points-on-a-line.js

--- Points-on-a-line.js
+++ Points-on-a-line.js
@@ -9,3 +9,13 @@ onLine([[1,2], [-3, -14], [22, 9]]);              // returns false
 
 
 // Solution
+
+function onLine(points) {
+  if (points.length < 3) return true;
+  let x = points.shift();
+  let a = undefined;
+  return points.
+    filter (function(y) { return !(x[0] == y[0] && x[1] == y[1]) }).
+    map    (function(y) { return a = (x[0] - y[0])/(x[1] - y[1]) }).
+    every  (function(y) { return y == a });
+}
