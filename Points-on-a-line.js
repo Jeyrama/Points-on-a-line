52192bd6a6ea5ea42f26c4a8f81c99cf150644b3
Jey <126204426+Jeyrama@users.noreply.github.com>
2024-05-23 19:07:06 -0700

    Update Points-on-a-line.js

--- Points-on-a-line.js
+++ Points-on-a-line.js
@@ -6,3 +6,6 @@ Treat both an empty set and a single point as a line.
 onLine([[1,2], [7, 4], [22, 9]]);                 // returns true
 onLine([[1,2], [-3, -14], [22, 9]]);              // returns false
 */
+
+
+// Solution
